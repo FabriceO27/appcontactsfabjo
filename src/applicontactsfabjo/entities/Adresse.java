d11cd8d9dbe42f340677647ac71275efedcde01b
Bernard Tapie <objois_fabrice@live.fr>
2019-12-16 15:40:42 +0100

    méthode crud dao et service de la class Adresse

--- src/applicontactsfabjo/entities/Adresse.java
+++ src/applicontactsfabjo/entities/Adresse.java
@@ -1,5 +1,71 @@
 package applicontactsfabjo.entities;
 
-public class Adresse {
+import java.io.Serializable;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "adresses")
+@Access(AccessType.FIELD)
+public class Adresse implements Serializable{
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pk;
+	@Column(name = "rue")
+	private String voie;
+	@Column(name = "code_postal")
+	private String codePostal;
+	private String ville;
+	private String pays;
+	
+	//CONSTRUCTEURS
+	public Adresse() {}
+	public Adresse(String voie, String codePostal, String ville, String pays) {
+		this.voie = voie;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.pays = pays;
+	}
+
+	//GETTERS SETTERS
+	public int getPk() {
+		return pk;
+	}
+	public void setPk(int pk) {
+		this.pk = pk;
+	}
+	public String getVoie() {
+		return voie;
+	}
+	public void setVoie(String voie) {
+		this.voie = voie;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	
 }
--- src/applicontactsfabjo/services/AdresseServices.java
+++ src/applicontactsfabjo/services/AdresseServices.java
@@ -1,5 +1,36 @@
 package applicontactsfabjo.services;
 
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+
+import applicontactsfabjo.dao.AdresseDao;
+import applicontactsfabjo.entities.Adresse;
+
+@Singleton
 public class AdresseServices {
 
+	@EJB
+	private AdresseDao dao;
+	
+	public List<Adresse> findAdresses(){
+		return dao.findAdresses();
+	}
+	
+	public List<Adresse> findAdresseByPk(int pk){
+		return dao.findAdresseByPk(pk);
+	}
+	
+	public void addAdresse(Adresse adresse) {
+		dao.addAdresse(adresse);
+	}
+	
+	public void updateAdresse(Adresse adresse, int pk) {
+		dao.updateAdresse(adresse, pk);
+	}
+	
+	public void deleteAdresse(int pk) {
+		dao.deleteAdresse(pk);
+	}
 }
