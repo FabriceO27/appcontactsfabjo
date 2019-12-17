fefaaa9d1748969355af3791a9d3ceeed3b4f6d5
lachkarmo <lachkarmo@hotmail.fr>
2019-12-17 14:30:37 +0100

    Reste Afficher et Modfier contact

---
+++ src/applicontactsfabjo/presentation/ModifierContact.java
@@ -0,0 +1,40 @@
+package applicontactsfabjo.presentation;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import applicontactsfabjo.services.PersonneServices;
+
+/**
+ * Servlet implementation class ModifierContact
+ */
+@WebServlet("/ModifierContact")
+public class ModifierContact extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private PersonneServices service;
+	private RequestDispatcher dispatcher;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		
+	
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
